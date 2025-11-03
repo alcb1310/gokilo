c2f98af0086719d6d62b872e96e985657b9dd752
Andres Court <andres@andrescourt.com>
2025-11-03 17:08:32 -0500

    Safe exit

--- main.go
+++ main.go
@@ -6,13 +6,36 @@ import (
 	"os"
 )
 
+type Global struct {
+	f func()
+}
+
+var E Global
+
+func (g *Global) safeExit(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\r\n", err)
+	}
+	if g.f != nil {
+		g.f()
+	}
+	os.Exit(0)
+}
+
+func initEditor() {
+	E = Global{
+		f: nil,
+	}
+}
 func main() {
-	safe, err := enableRawMode()
+	initEditor()
+	var err error
+	E.f, err = enableRawMode()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error enabling raw mode: %v\r\n", err)
-		os.Exit(1)
+		defer E.safeExit(err)
 	}
-	defer safe()
+	defer E.safeExit(nil)
 
 	reader := bufio.NewReader(os.Stdin)
 
