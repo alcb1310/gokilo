96bf3c7f6a3f23e9ff4536547f85cf8e87839a40
Andres Court <andres@andrescourt.com>
2025-11-03 14:49:45 -0500

    Press 'q' to quit

--- main.go
+++ main.go
@@ -10,10 +10,14 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
-		_, err := reader.ReadByte()
+		b, err := reader.ReadByte()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error reading input: %v\n", err)
 			os.Exit(1)
+		}
+
+		if b == 'q' {
+			break
 		}
 	}
 }
